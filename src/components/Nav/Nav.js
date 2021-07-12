d9dccb9e4a9efba42ae46ef9e4cd339007b18bd5
Jonah Tarver <jonahdevine@gmail.com>
2021-07-11 17:46:43 -0700

    Fix nav and Sect4 details

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import styles from './Nav.module.css';
 
-const Nav = ({home}) => {
+const Nav = ({home, about}) => {
     return (
         <nav className={styles.nav}>
             <NavLink className={home ? `${styles.active} ${styles.link}`: styles.link} to={'/'}>projects.</NavLink>
-            <NavLink className={home ? styles.link : `${styles.active} ${styles.link}`} to={'/about'}>about.</NavLink>
+            <NavLink className={about ? `${styles.active} ${styles.link}`: styles.link} to={'/about'}>about.</NavLink>
             <a className={styles.link} href={'mailto:daniellekryce@gmail.com'}>contact.</a>
         </nav>
     )
