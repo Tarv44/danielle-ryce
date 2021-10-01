da7ba5a39df9f69ba626379050b8dffbe5fcaddc
Jonah Tarver <jonahdevine@gmail.com>
2021-10-01 13:31:55 -0700

    Fix image sizing

--- src/components/CSIntro/CSIntro.js
+++ src/components/CSIntro/CSIntro.js
@@ -1,37 +1,35 @@
-import React, { useContext, useEffect, useState } from 'react';
-import styles from './CSIntro.module.css';
-import MobileContext from '../../mobile.context';
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./CSIntro.module.css";
+import MobileContext from "../../mobile.context";
 
 const Intro = (props) => {
-  const {isMobile} = useContext(MobileContext)
-  const [useMobile, setUseMobile] = useState(isMobile)
+  const { isMobile } = useContext(MobileContext);
+  const [useMobile, setUseMobile] = useState(isMobile);
+  const sumImage = (
+    <img
+      className={styles.img}
+      src={props.img}
+      alt={props.alt}
+      style={{ background: props.color }}
+    />
+  );
   useEffect(() => {
-    setUseMobile(isMobile)
-  }, [isMobile])
+    setUseMobile(isMobile);
+  }, [isMobile]);
   return (
     <section className={styles.intro}>
       <div className={styles.header}>
-        <h2 style={{maxWidth: props.titleWidth}}>{props.title}</h2>
+        <h2 style={{ maxWidth: props.titleWidth }}>{props.title}</h2>
         <h4>Role: {props.role}</h4>
       </div>
       <div className={styles.content}>
-        {!useMobile && <img
-          className={styles.img} 
-          src={props.img} 
-          alt={props.alt}
-          style={{background: props.color}} 
-        />}
+        {!useMobile && sumImage}
         <div className={styles.summary}>
           <div>
             <h3>{props.title1 || "Background"}</h3>
             <p>{props.background}</p>
           </div>
-          {useMobile && <img
-            className={styles.img} 
-            src={props.img} 
-            alt={props.alt}
-            style={{background: props.color}} 
-          />}
+          {useMobile && sumImage}
           <div>
             <h3>{props.title2 || "Objective"}</h3>
             <p>{props.objective}</p>
@@ -39,7 +37,7 @@ const Intro = (props) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Intro;
